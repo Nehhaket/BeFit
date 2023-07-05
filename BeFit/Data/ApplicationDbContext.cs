9258641d806bb05d8ec9441c1eb29f07f967d4b0
Karol <theorangebox3@gmail.com>
2023-07-05 18:49:04 +0200

    chore: seed admin role and user

--- BeFit/Data/ApplicationDbContext.cs
+++ BeFit/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BeFit.Data
@@ -8,6 +9,45 @@ namespace BeFit.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            string ADMIN_USER_ID = "02174cf0–9412–4cfe-afbf-59f706d72cf6";
+            string ADMIN_ROLE_ID = "341743f0-asd2–42de-afbf-59kmkkmk72cf6";
+
+            // seed admin role
+            builder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Name = "SuperAdmin",
+                NormalizedName = "SUPERADMIN",
+                Id = ADMIN_ROLE_ID,
+                ConcurrencyStamp = ADMIN_ROLE_ID
+            });
+
+            // seed admin user
+            var adminUser = new IdentityUser
+            {
+                Id = ADMIN_USER_ID,
+                Email = "admin@gmail.com",
+                EmailConfirmed = true,
+                UserName = "admin@gmail.com",
+                NormalizedUserName = "ADMIN@GMAIL.COM"
+            };
+
+            // set admin user password
+            PasswordHasher<IdentityUser> ph = new PasswordHasher<IdentityUser>();
+            adminUser.PasswordHash = ph.HashPassword(adminUser, "Adminadmin1!");
+
+            //seed user
+            builder.Entity<IdentityUser>().HasData(adminUser);
+
+            // give admin role to admin user
+            builder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                RoleId = ADMIN_ROLE_ID,
+                UserId = ADMIN_USER_ID
+            });
         }
     }
 }
---
+++ BeFit/Data/Migrations/20230705171849_razdwatrzy.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeFit.Data.Migrations
+{
+    public partial class razdwatrzy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "341743f0-asd2–42de-afbf-59kmkkmk72cf6", "341743f0-asd2–42de-afbf-59kmkkmk72cf6", "SuperAdmin", "SUPERADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "02174cf0–9412–4cfe-afbf-59f706d72cf6", 0, "52e43f74-3c24-4836-a392-6ace8f939947", "admin@gmail.com", true, false, null, null, "ADMIN@GMAIL.COM", "AQAAAAEAACcQAAAAEHjy6MNRpqD5tS96rxtDGlsuzLFIgW9MnrUgRo4YhkkV8vLS8pk6LzTcTck2n82RfQ==", null, false, "af53a12b-404a-44cc-8af6-d92c869bd492", false, "admin@gmail.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "341743f0-asd2–42de-afbf-59kmkkmk72cf6", "02174cf0–9412–4cfe-afbf-59f706d72cf6" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "341743f0-asd2–42de-afbf-59kmkkmk72cf6", "02174cf0–9412–4cfe-afbf-59f706d72cf6" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "341743f0-asd2–42de-afbf-59kmkkmk72cf6");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "02174cf0–9412–4cfe-afbf-59f706d72cf6");
+        }
+    }
+}
