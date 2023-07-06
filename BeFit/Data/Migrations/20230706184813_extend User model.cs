194d8c82eb53d7afdafa733c589da51f843fd2e6
Karol <theorangebox3@gmail.com>
2023-07-06 20:48:32 +0200

    feat: extend User model

---
+++ BeFit/Data/Migrations/20230706184813_extend User model.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeFit.Data.Migrations
+{
+    public partial class extendUsermodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Height",
+                table: "User",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "02174cf0–9412–4cfe-afbf-59f706d72cf6",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "502422f8-b808-4b6a-921b-9f30c0c6bc02", "AQAAAAEAACcQAAAAEJR84Mq+GaVBY8KXVNv1QRrXaOAoE/sqoh2R+rmWrV/sYLlccCIlzWDH4ULfnASt8A==", "22a6045b-0517-4d0a-9111-37a43af2cbc9" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "User");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "02174cf0–9412–4cfe-afbf-59f706d72cf6",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "c1d078c3-4314-4ac4-be32-4e30b9dde1cd", "AQAAAAEAACcQAAAAEKcAgsM7FqNGt8pMYau7IDceTzPMveWqNIg7c5eerQ+t8GNqy1S0IFcrbVG0NkG9Jw==", "319507e0-df55-49e5-921a-43cc5c330275" });
+        }
+    }
+}
--- BeFit/Models/User.cs
+++ BeFit/Models/User.cs
@@ -5,6 +5,9 @@ namespace BeFit.Models
     public class User
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public float Height { get; set; }
         public int IdentityUserId { get; set; }
         public virtual IdentityUser IdentityUser { get; set; }
     }
